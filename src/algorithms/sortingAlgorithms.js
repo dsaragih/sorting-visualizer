94387dabced2261573210675a0993076b871dfca
dsaragih <66550359+dsaragih@users.noreply.github.com>
2021-08-26 13:29:56 +0700

    Update sortingAlgorithms.js

--- src/algorithms/sortingAlgorithms.js
+++ src/algorithms/sortingAlgorithms.js
@@ -1,9 +1,9 @@
 export function* bubbleSort (array) {
     for (let i = 0; i < array.length - 1; i++) {
         for (let j = 0; j < array.length - i - 1; j++) {
-			if (array[j] > array[j + 1]) {
-				[array[j], array[j + 1]] = [array[j + 1], array[j]];
-			}
+	    if (array[j] > array[j + 1]) {
+	    [array[j], array[j + 1]] = [array[j + 1], array[j]];
+	    }
             yield j;
         }
     }  
@@ -67,8 +67,8 @@ export function* mergeSort (array, start=0, end=200) {
         }
         
         while (j < right.length) {
-			array[k] = right[j];
-			j++;
+	    array[k] = right[j];
+	    j++;
             k++;
             yield j;
             yield k;
